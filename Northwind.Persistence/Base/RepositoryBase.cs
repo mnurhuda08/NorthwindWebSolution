a3ffb3565d938a3f684e84c30f77ca1ea2ccaa74
mnurhuda08 <mnurhuda009@gmail.com>
2023-12-31 19:12:50 +0700

    continue persistance layer

---
+++ Northwind.Persistence/Base/RepositoryBase.cs
@@ -0,0 +1,79 @@
+﻿using Northwind.Persistence.Interface;
+using Northwind.Persistence.RepositoryContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Base
+{
+    internal abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected AdoDbContext _adoContext;
+
+        protected RepositoryBase(AdoDbContext adoContext) {
+            _adoContext = adoContext;
+        }
+
+        public void Create(SqlCommandModel model) {
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+
+        }
+
+        public void Delete(SqlCommandModel model)
+        {
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+        }
+
+        public IEnumerator<T> FindAll<T>(string sql)
+        {
+            var listOfData = _adoContext.ExecuteReader<T>(sql);
+            _adoContext.Dispose();
+            return listOfData;
+        }
+
+
+        public IAsyncEnumerator<T> FindAllAsync<T>(SqlCommandModel model)
+        {
+            var dataT = _adoContext.ExecuteReaderAsync<T>(model);
+            _adoContext.DisposeAsync();
+            return dataT;
+        }
+
+
+        public IEnumerator<T> FindByCondition<T>(SqlCommandModel model)
+        {
+            var listOfData = _adoContext.ExecuteReader<T>(model);
+            _adoContext.Dispose();
+            return listOfData;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync<T>(SqlCommandModel model)
+        {
+            var dataT = _adoContext.ExecuteReaderAsync<T>(model);
+
+            var listData = new List<T>();
+
+            while (await dataT.MoveNextAsync())
+            {
+                listData.Add(dataT.Current);
+            }
+
+            _adoContext.DisposeAsync();
+
+            return listData;
+        }
+
+        public void Update(SqlCommandModel model)
+        {
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+        }
+
+
+
+    }
+}
---
+++ Northwind.Persistence/Interface/IRepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using Northwind.Persistence.RepositoryContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Interface
+{
+    public interface IRepositoryBase<T>
+    {
+        IEnumerator<T> FindAll<T>(string sql);
+        IEnumerator<T> FindByCondition<T>(SqlCommandModel model);
+        IAsyncEnumerator<T> FindAllAsync<T>(SqlCommandModel model);
+        Task<IEnumerable<T>> GetAllAsync<T>(SqlCommandModel model);
+
+
+        void Create(SqlCommandModel model);
+        void Update(SqlCommandModel model);
+        void Delete(SqlCommandModel model);
+    }
+}
