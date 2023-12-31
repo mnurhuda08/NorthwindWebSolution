51d9d90297df49ee4c6b813c79f49b939c81a4c2
mnurhuda08 <mnurhuda009@gmail.com>
2023-12-31 20:38:44 +0700

    Repository Implementation

---
+++ Northwind.Persistence/Base/RepositoryManager.cs
@@ -0,0 +1,69 @@
+﻿using Northwind.Domain.Base;
+using Northwind.Domain.Repositories;
+using Northwind.Persistence.Repositories;
+using Northwind.Persistence.RepositoryContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Base
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private AdoDbContext _adoContext;
+        private IRegionRepository _regionRepository;
+/*
+        private IProductRepository _productRepository;
+
+        private Lazy<IProductPhotoRepository> _productPhotoRepositoryLazy;
+
+        private Lazy<ISupplierRepository> _supplierRepositoryLazy;
+
+        private Lazy<IUserRepository> _userRepositoryLazy;*/
+
+        //private Lazy<IRegionRepository> _regionRepositoryLazy;
+
+        public RepositoryManager(AdoDbContext adoContext)
+        {
+            _adoContext = adoContext;
+           // _regionRepositoryLazy = new Lazy<IRegionRepository>(()=> new RegionRepository(adoContext));
+            /*_productPhotoRepositoryLazy = new Lazy<IProductPhotoRepository>(() => new ProductPhotoRepository(adoContext));
+            _supplierRepositoryLazy = new Lazy<ISupplierRepository>(() => new SupplierRepository(adoContext));
+            _userRepositoryLazy = new Lazy<IUserRepository>(() => new UsersRepository(adoContext));*/
+        }
+
+        //public IRegionRepository RegionRepository => _regionRepositoryLazy.Value;
+
+        public IRegionRepository RegionRepository
+        {
+            get
+            {
+                if (_regionRepository == null)
+                {
+                    _regionRepository = new RegionRepository(_adoContext);
+                }
+                return _regionRepository;
+            }
+        }
+/*
+        public IProductRepository ProductRepository
+        {
+            get
+            {
+                if (_productRepository == null)
+                {
+                    _productRepository = new ProductRepository(_adoContext);
+                }
+                return _productRepository;
+            }
+        }*/
+/*
+        public IProductPhotoRepository ProductPhotoRepository => _productPhotoRepositoryLazy.Value;
+
+        public ISupplierRepository SupplierRepository => _supplierRepositoryLazy.Value;
+
+        public IUserRepository UserRepository => _userRepositoryLazy.Value;*/
+    }
+}
---
+++ Northwind.Persistence/Repositories/RegionRepository.cs
@@ -0,0 +1,129 @@
+﻿using Northwind.Domain.Entities;
+using Northwind.Domain.Repositories;
+using Northwind.Persistence.Base;
+using Northwind.Persistence.RepositoryContext;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Repositories
+{
+    internal class RegionRepository : RepositoryBase<Region>, IRegionRepository
+    {
+        public RegionRepository(AdoDbContext adoContext) : base(adoContext)
+        {
+
+        }
+
+        public void Edit(Region region)
+        {
+            SqlCommandModel model = new SqlCommandModel()
+            {
+                CommandText = "UPDATE region SET regionDescription=@regionDescription WHERE regionId= @regionId;",
+                CommandType = CommandType.Text,
+                CommandParameters = new SqlCommandParameterModel[] {
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionId",
+                        DataType = DbType.Int32,
+                        Value = region.RegionId
+                    },
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionDescription",
+                        DataType = DbType.String,
+                        Value = region.RegionDescription
+                    }
+                }
+            };
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+         }
+
+        public IEnumerable<Region> FindAllRegion() {
+            IEnumerator<Region> dataSet = FindAll<Region>("SELECT RegionID,RegionDescription FROM dbo.region");
+            while (dataSet.MoveNext())
+            {
+                var data = dataSet.Current;
+                yield return data;
+
+            }
+        }
+
+        public Task<IEnumerable<Region>> FindAllRegionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Region FindRegionByI(int Id)
+        {
+            SqlCommandModel model = new SqlCommandModel()
+            {
+                CommandText = "SELECT RegionId,RegionDescription FROM DBO.region where regionId=@regionId;",
+                CommandType = CommandType.Text,
+                CommandParameters = new SqlCommandParameterModel[] {
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionId",
+                        DataType = DbType.Int32,
+                        Value = id
+                    }
+                }
+            };
+
+            var dataSet = FindByCondition<Region>(model);
+
+            Region? item = dataSet.Current;
+
+            while (dataSet.MoveNext())
+            {
+                item = dataSet.Current;
+            }
+
+
+            return item;
+        }
+        public void Insert(Region region)
+        {
+            SqlCommandModel model = new SqlCommandModel()
+            {
+                CommandText = "INSERT INTO region (regionId,RegionDescription) values (@regionId,@regionDescription);",
+                CommandType = CommandType.Text,
+                CommandParameters = new SqlCommandParameterModel[] {
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionId",
+                        DataType = DbType.Int32,
+                        Value = region.RegionId
+                    },
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionDescription",
+                        DataType = DbType.String,
+                        Value = region.RegionDescription
+                    }
+                }
+            };
+
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+        }
+
+        public void Remove(Region region)
+        {
+            SqlCommandModel model = new SqlCommandModel()
+            {
+                CommandText = "DELETE FROM region WHERE regionId=@regionId;",
+                CommandType = CommandType.Text,
+                CommandParameters = new SqlCommandParameterModel[] {
+                    new SqlCommandParameterModel() {
+                        ParameterName = "@regionId",
+                        DataType = DbType.Int32,
+                        Value = region.RegionId
+                    }
+                }
+            };
+
+            _adoContext.ExecuteNonQuery(model);
+            _adoContext.Dispose();
+        }
+    }
+}
