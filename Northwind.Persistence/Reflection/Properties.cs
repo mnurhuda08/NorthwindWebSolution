bdd23360452a5246cb6f24129465694c2d6d885a
mnurhuda08 <mnurhuda009@gmail.com>
2023-12-31 18:54:27 +0700

    Add project files.

---
+++ Northwind.Persistence/Reflection/Properties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Reflection
+{
+    internal class Properties
+    {
+        public static PropertyInfo GetProperty<T>(string name)
+        {
+            return typeof(T).GetProperty(name);
+        }
+
+        public static T GetValue<T, U>(U source, string name)
+        {
+            return (T)GetProperty<U>(name).GetValue(source);
+        }
+
+        public static int GetFieldCount(DbDataRecord record)
+        {
+            return GetValue<int, DbDataRecord>(record, "FieldCount");
+        }
+    }
+}
---
+++ Northwind.Persistence/RepositoryContext/SqlCommandModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.RepositoryContext
+{
+    public class SqlCommandModel
+    {
+        public string CommandText { get; set; }
+        public CommandType CommandType { get; set; }
+        public SqlCommandParameterModel[] CommandParameters {  get; set; }
+    }
+}
